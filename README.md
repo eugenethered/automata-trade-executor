5f19a662d14b02ae21cc8111ace216614be7a824
Eugene the Red <me@eugenethered.com>
2022-05-05 20:39:45 +0100

    using trade repo

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 `python3 -m build`
 
 ## Dependencies (IDE Terminal)
-`pip install persuader-technology-automata-core persuader-technology-automata-utilities persuader-technology-automata-redis`
+`pip install persuader-technology-automata-trade-repository`
--- trade/TradeConductor.py
+++ trade/TradeConductor.py
@@ -1,37 +1,31 @@
 import logging
 
-from cache.holder.RedisCacheHolder import RedisCacheHolder
 from core.trade.InstrumentTrade import InstrumentTrade, Status
+from traderepo.repository.TradeRepository import TradeRepository
 
 from trade.executor.TradeExecutor import TradeExecutor
-from trade.serialize.trade_deserializer import deserialize_trade
-from trade.serialize.trade_serializer import serialize_trade
 
 
 class TradeConductor:
 
-    def __init__(self, options, trade_executor: TradeExecutor):
+    def __init__(self, options, trade_repository: TradeRepository, trade_executor: TradeExecutor):
         self.options = options
-        self.cache = RedisCacheHolder()
+        self.trade_repository = trade_repository
         self.trade_executor = trade_executor
 
     def build_trade_key(self):
         return self.options['TRADE_KEY']
 
-    def store_trade_to_execute(self, trade: InstrumentTrade):
-        trade_key = self.build_trade_key()
-        trade_to_store = serialize_trade(trade)
-        self.cache.store(trade_key, trade_to_store)
+    def store_submitted_trade(self, trade: InstrumentTrade):
+        self.trade_repository.store_trade(trade)
 
-    def fetch_trade_to_execute(self) -> InstrumentTrade:
-        trade_key = self.build_trade_key()
-        raw_trade = self.cache.fetch(trade_key, as_type=dict)
-        return deserialize_trade(raw_trade)
+    def fetch_trade_to_submit(self) -> InstrumentTrade:
+        return self.trade_repository.retrieve_trade()
 
     def perform_trade(self):
-        trade = self.fetch_trade_to_execute()
+        trade = self.fetch_trade_to_submit()
         if trade.status == Status.NEW:
             updated_trade = self.trade_executor.trade(trade)
-            self.store_trade_to_execute(updated_trade)
+            self.store_submitted_trade(updated_trade)
         else:
             logging.warning(f'Trade is not new, so will not trade -> {trade}')
